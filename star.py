7e42879a6fa348c04f0de698381346886ace485a
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-09 09:23:52 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,8 +1,6 @@
-i=0
-j=0
-while(i>13):
-    while(j>13):
-        print(" * ")
-        j+=1
-    i+=1
-    print("\n")
+for i in range(0,13):
+    for j in range(0,13):
+        if(j<6 && m<=j || j>=6 && j<=n):
+        print(" *", end="")
+    
+    print("")
