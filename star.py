4262088d96b9f9a64aeab23f894d62ef28177435
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-09 09:44:31 +0530

    Update star.py

--- star.py
+++ star.py
@@ -2,5 +2,6 @@ for i in range(0,13):
     for j in range(0,13):
         if(j<5):
             print(" *", end="")
-        else: print(" ")
+        else: 
+            print(" ",end="")
     print("")
