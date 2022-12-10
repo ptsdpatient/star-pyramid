a06e28bf53d5db37026ba3350f747b7ae20cb6f5
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-10 11:08:49 +0530

    C# script added

---
+++ star/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace pyramid{
+    class star{
+        public static void Main(string[] args){
+           int m=6,n=6;
+            for(int i=0;i<13;i++){
+                for(int j=0; j<13;j++){
+                    if(j<6 && j==m || j<6 && j==n){               
+                    Console.Write(" *");} else Console.Write("   ");
+                }
+                if(i%2==1){
+                    m++;
+                    n--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
