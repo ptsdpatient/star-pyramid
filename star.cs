1ee12b0079cfbeae831a15adc957426c4feb8fd4
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-10 11:12:57 +0530

    C# final

--- star.cs
+++ star.cs
@@ -5,14 +5,15 @@ namespace pyramid{
            int m=6,n=6;
             for(int i=0;i<13;i++){
                 for(int j=0; j<13;j++){
-                    if(j<6 && j==m || j<6 && j==n){               
-                    Console.Write(" *");} else Console.Write("   ");
+                    if(j<6 && j>=m || j>=6 && j<=n){               
+                    Console.Write(" * ");} else Console.Write("   ");
                 }
+                Console.WriteLine(" ");
                 if(i%2==1){
-                    m++;
-                    n--;
+                    m--;
+                    n++;
                 }
-                Console.WriteLine();
+                
             }
         }
     }
