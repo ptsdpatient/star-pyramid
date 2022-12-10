43565689812fbb1b885eb60a75bd1ef333fe0241
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-10 08:38:26 +0530

    Update star.cpp

--- star.cpp
+++ star.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int i,j,m=6,n=6;//i is row, j is column,k is var used to determine which element it shouldn't print
-    //k ka ab tak istmal hi nahi kar parahahu
+   
     for(i=0;i<14;i++)
     { 
       for(j=0;j<16;j++)
